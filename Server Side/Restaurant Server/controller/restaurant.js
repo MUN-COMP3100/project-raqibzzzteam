0437694c9aec132859c4146abe2fc224b3f95465
VilakshanKh <31506996+Vilakshankh@users.noreply.github.com>
2023-03-14 11:53:28 -0230

    Update restaurant.js

---
+++ Server Side/Restaurant Server/controller/restaurant.js
@@ -0,0 +1,21 @@
+import fs from  'fs' ;
+import client from '../utils/db.mjs';
+import { Restaurant } from '../model/restaurant.mjs';
+
+
+/**
+ * A function that adds a restaurant to the database.
+ * @param {Request} req - A request Object
+ * @param {Response} res - A response Object
+ */
+
+export async function add(req, res) 
+{
+    let name = req.body.name;
+    let cuisine = req.body.cuisine;
+    let location = req.body.location;
+    let new_restaurant = new Restaurant(name, cuisine, location);
+    let msg = await new_restaurant.save();
+    res.send(msg);                
+}
+
