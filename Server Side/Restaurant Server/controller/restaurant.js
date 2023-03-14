c73bc3866098821bcf7a748b670356a92347e0e0
VilakshanKh <31506996+Vilakshankh@users.noreply.github.com>
2023-03-14 11:57:32 -0230

    Update restaurant.js

--- Server Side/Restaurant Server/controller/restaurant.js
+++ Server Side/Restaurant Server/controller/restaurant.js
@@ -19,6 +19,63 @@ export async function add(req, res)
     res.send(msg);                
 }
 
+/**
+ * A function that lists all restaurants with all information that is
+ * in the file. 
+ * @param {Request} req - A request Object
+ * @param {Response} res - A response Object
+ */
+
+export async function list_all(req, res)
+{
+    let objs = await Restaurant.getAll();
+    console.log(objs.length+' item(s) sent.');
+    res.send(objs);        
+}
+
+/**
+ * A function that gets a restaurant by name and returns all
+ * data of the requested contact. 
+ * @param {Request} req - A request Object
+ * @param {Response} res - A response Object
+ */
+
+export async function get_restaurant(req, res)
+{
+    let name_to_match = req.params.name;
+    let obj = await Restaurant.get(name_to_match);
+    if (obj.length > 0){
+        console.log(obj.length+' item(s) sent.');
+        res.send(obj[0]);        
+    }else{
+        res.send('No item was found');
+    }
+    
+}
+
+/**
+ * A function that deletes the information about a given restaurant.
+ * @param {Request} req - A request Object
+ * @param {Response} res - A response Object
+ */
+
+export async function delete_restaurant(req, res)
+{
+    let name_to_match = req.params.name;
+    let obj = await Restaurant.get(name_to_match);
+    if (obj.length > 0){
+        let msg = await Restaurant.delete(name_to_match);
+        res.send(msg);        
+    }else{
+        res.send('No item was found');
+    }
+    
+}
+/**
+ * A function to update the information about a given restaurant.
+ * @param {Request} req - A request Object
+ * @param {Response} res - A response Object
+ */
 export async function update_restarant(req, res) {
     let name = req.body.name;
     let cuisine = req.body.cuisine;
