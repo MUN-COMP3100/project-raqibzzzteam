71c68f11fb1da7b0a4b08068ada32bdda6b8aaf9
raqibzzz <89846978+raqibmuktadir@users.noreply.github.com>
2023-04-08 15:06:17 -0230

    Google Maps first commit

---
+++ Client Side/view/restaurant.js
@@ -0,0 +1,28 @@
+function loadTable(data) {
+    // contentView.style.display = "block";
+    let table = document.getElementById("resultTable");
+    table.innerHTML = "";
+    let header = `<tr>
+        <th>Name</th>
+        <th>Cuisine</th>
+        <th>Location</th>
+        <th>Mood</th>
+        <th>Rating</th>
+        </tr>`;
+        table.innerHTML += header;
+        let row = `<tr>
+        <td>${data.Name}</td>
+        <td>${data.Cuisine}</td>
+        <td>${data.Location}</td>
+        <td>${data.Mood}</td>
+        <td>${data.Rating}</td>`;
+        table.innerHTML += row;
+}
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 37.7749, lng: -122.4194},
+      zoom: 10
+    });
+  }
+  
