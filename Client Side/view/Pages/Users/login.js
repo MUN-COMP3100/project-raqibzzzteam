33eb4c0dbc74433c6c3a66c3cc23f556f3e4f0c1
raqibzzz <89846978+raqibmuktadir@users.noreply.github.com>
2023-04-13 14:07:19 -0230

    update login

---
+++ Client Side/view/Pages/Users/login.js
@@ -0,0 +1,21 @@
+const baseurl="http://localhost:3001";
+
+function login(){
+    let username=document.getElementById("username").value;
+    let password=document.getElementById("password").value;
+    fetch(`${baseurl}/users`)
+    .then(response=>response.json())
+    .then((data)=>
+    {
+        let user = data.find((user) => user.username === username && user.password === password);
+        if (user) {
+            console.log("Login successful");
+            // Redirect to the homepage or the user's dashboard
+        } else {
+            console.log("Invalid username or password");
+        }
+    });
+}
+
+const loginbtn=document.getElementById("loginbtn");
+loginbtn.addEventListener("click",login);
