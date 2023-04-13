bed583a5226a599f9c1177584daeff0a8d4225a6
raqibzzz <89846978+raqibmuktadir@users.noreply.github.com>
2023-04-13 14:46:25 -0230

    update

--- Client Side/view/Pages/Users/login.js
+++ Client Side/view/Pages/Users/login.js
@@ -1,5 +1,15 @@
 const baseurl="http://localhost:3001";
 
+function find(){
+    fetch(`${baseurl}/users`)
+    .then(response=>response.json())
+    .then((data)=>
+    {
+        loadList(data);
+        console.log(data);
+    });
+  }
+
 function login(){
     let username=document.getElementById("username").value;
     let password=document.getElementById("password").value;
